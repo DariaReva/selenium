d749a664fc9028d8579e21230cbe9a1f8d75505e
DReva <DReva@DREVA>
2018-04-28 18:29:19 +0300

    task7

---
+++ seleniumProject/seleniumProject/Task7.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+
+namespace seleniumProject
+{
+    [TestClass]
+    public class Task7
+    {
+        IWebDriver Browser = new OpenQA.Selenium.Firefox.FirefoxDriver();
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Browser.Manage().Window.Maximize();
+        }
+        
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Browser.Navigate().GoToUrl("http://localhost/litecart/admin/");
+            IWebElement input_login = Browser.FindElement(By.Name("username"));
+            input_login.Click();
+            input_login.SendKeys("admin");
+
+            IWebElement input_password = Browser.FindElement(By.Name("password"));
+            input_password.Click();
+            input_password.SendKeys("admin");
+
+            IWebElement enter = Browser.FindElement(By.Name("login"));
+            enter.Click();
+
+            IList<IWebElement> menu = Browser.FindElements(By.XPath(".//*[@id='app-']/a"));
+            for (int i = 0; i < menu.Count; i++)
+            {
+                menu[i].Click();
+                IList<IWebElement> element = Browser.FindElements(By.CssSelector("[href*='http://localhost/litecart/admin/?app=']"));
+                for (int j = 0; j < element.Count; j++)
+                    element[j].Click();
+                Browser.Navigate().GoToUrl("http://localhost/litecart/admin/");
+            }
+        }
+        [TestCleanup]
+        public void Quit()
+        {
+            //Browser.Quit();
+        }
+    }
+}
