b39764ac264b1701b34ce2382944a4792dad14da
DReva <DReva@DREVA>
2018-04-28 14:52:35 +0300

    chrome

--- seleniumProject/seleniumProject/UnitTest1.cs
+++ seleniumProject/seleniumProject/UnitTest1.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
 
 namespace seleniumProject
 {
     [TestClass]
     public class UnitTest1
     {
+        IWebDriver Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Browser.Manage().Window.Maximize();
+        }
         [TestMethod]
         public void TestMethod1()
         {
+            Browser.Navigate().GoToUrl("http://localhost/litecart/admin/");
+            IWebElement input_login = Browser.FindElement(By.Name("username"));
+            input_login.Click();
+            input_login.SendKeys("admin");
+
+            IWebElement input_password = Browser.FindElement(By.Name("password"));
+            input_password.Click();
+            input_password.SendKeys("admin");
+
+            IWebElement enter = Browser.FindElement(By.Name("login"));
+            enter.Click();
+        }
+        [TestCleanup]
+        public void Quit()
+        {
+            Browser.Quit();
         }
     }
 }
